25682498b42e18f57b327315e97cb38f3cc5a4c8
liufule12 <liufule12@gmali.com>
2014-06-14 11:26:51 +0800

    add the frequency method.

--- bioHelper/util.py
+++ bioHelper/util.py
@@ -15,6 +15,29 @@ class Seq:
         return ">%s\tNo:%s\tlength:%s\n%s" % (self.name, str(self.no), str(self.length), self.seq)
 
 
+def frequency(tol_str, tar_str):
+    """
+    Generate the frequency of tar_str in tol_str.
+    Return the count.
+    """
+    i, j, tar_count = 0, 0, 0
+    len_tol_str = len(tol_str)
+    len_tar_str = len(tar_str)
+    while i < len_tol_str and j < len_tar_str:
+        if tol_str[i] == tar_str[j]:
+            i += 1
+            j += 1
+            if j >= len_tar_str:
+                tar_count += 1
+                i = i - j + 1
+                j = 0
+        else:
+            i = i - j + 1
+            j = 0
+
+    return tar_count
+
+
 def is_format_legal(seq, alphabet):
     """
     Judge the seq is in alphabet or null.
