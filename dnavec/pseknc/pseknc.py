34e7c8eb894edf5d7abcff59545a4635b5116754
liufule12 <liufule12@gmali.com>
2014-07-14 11:43:45 +0800

    add the use example.

--- dnavec/pseknc/pseknc.py
+++ dnavec/pseknc/pseknc.py
@@ -1,9 +1,8 @@
 __author__ = 'aleeee'
 
-import cPickle
 import sys
 
-from dnavec.util import get_data, write_libsvm
+from dnavec.util import get_data
 
 DIPYHYCHE_LIST = ['Base stacking', 'Protein induced deformability', 'B-DNA twist', 'Dinucleotide GC Content',
                   'A-philicity', 'Propeller twist', 'Duplex stability:(freeenergy)', 'Duplex tability(disruptenergy)',
@@ -25,31 +24,48 @@ class PseKNC():
         self.w = w
         self.alphabet = alphabet
 
-    def make_vector(self, data, vec_type, phyche_list, added='none'):
+    def make_vector(self, data, phyche_list, vec_type=1, all_property=False):
         """Make a PseKNC vector.
 
         :param data: file object or sequence list.
         :param vec_type: 1 or 2.
         :param phyche_list: physicochemical properties list.
-        :param added: choose all physicochemical properties or not.
+        :param all_property: choose all physicochemical properties or not.
         :return: PseKNC vector.
 
         Examples
         --------
 
         >>> from dnavec.pseknc.pseknc import PseKNC
-        >>> pseKNC = PseKNC(k=2, lamada=1, w=0.05)
-        >>> pseKNC.make_vector(data=['GACTGAACTGCACTTTGGTTTCATATTATTTGCTC'], vec_type=1, phyche_list=['Twist', 'Tilt'])
-        [[0.027, 0.08, 0.0, 0.08, 0.053, 0.0, 0.0, 0.106, 0.053, 0.053, 0.027, 0.027, 0.053, 0.053, 0.106, 0.186, 0.095]]
 
-        >>> pseKNC.make_vector(data=['GACTGAACTGCACTTTGGTTTCATATTATTTGCTC'], vec_type=1, phyche_list=[], added='all')
+        >>> pseKNC = PseKNC(k=2, lamada=1, w=0.05)
+        >>> pseKNC.make_vector(data=['GACTGAACTGCACTTTGGTTTCATATTATTTGCTC'], phyche_list=['Twist', 'Tilt'])
         [[0.027, 0.081, 0.0, 0.081, 0.054, 0.0, 0.0, 0.108, 0.054, 0.054, 0.027, 0.027, 0.054, 0.054, 0.108, 0.19, 0.078]]
 
+        >>> pseKNC.make_vector(data=['GACTGAACTGCACTTTGGTTTCATATTATTTGCTC'], vec_type=2, phyche_list=[], all_property=True)
+        [[0.026, 0.079, 0.0, 0.079, 0.052, 0.0, 0.0, 0.105, 0.052, 0.052, 0.026, 0.026, 0.052, 0.052, 0.105, 0.183, 0.001,
+         -0.005, -0.005, 0.023, 0.008, 0.016, 0.017, 0.004, 0.001, 0.017, -0.015, -0.01, -0.004, 0.004, -0.008, -0.009,
+         0.023, -0.015, 0.004, 0.008, -0.015, 0.023, 0.017, -0.006, -0.007, -0.003, 0.006, 0.001, 0.017, 0.008, 0.004,
+         0.023, -0.008, -0.009, 0.036, 0.009, -0.022, -0.022]]
+
+        >>> pseKNC = PseKNC(k=3, lamada=1, w=0.05)
+        >>> pseKNC.make_vector(data=['GACTGAACTGCACTTTGGTTTCATATTATTTGCTC'], phyche_list=['Dnase I'])
+        [[0.0, 0.027, 0.0, 0.0, 0.0, 0.0, 0.0, 0.082, 0.0, 0.0, 0.0, 0.0, 0.027, 0.0, 0.0, 0.054, 0.0, 0.027, 0.0, 0.027,
+        0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.027, 0.054, 0.027, 0.027, 0.027, 0.0, 0.0, 0.027, 0.0, 0.0, 0.027,
+        0.0, 0.0, 0.0, 0.027, 0.0, 0.0, 0.0, 0.027, 0.0, 0.0, 0.0, 0.054, 0.027, 0.0, 0.0, 0.0, 0.027, 0.054, 0.027, 0.0,
+        0.027, 0.027, 0.054, 0.082, 0.102]]
+
+        >>> pseKNC.make_vector(data=['GACTGAACTGCACTTTGGTTTCATATTATTTGCTC'], vec_type=2, phyche_list=['Dnase I'])
+        [[0.0, 0.031, 0.0, 0.0, 0.0, 0.0, 0.0, 0.092, 0.0, 0.0, 0.0, 0.0, 0.031, 0.0, 0.0, 0.061, 0.0, 0.031, 0.0, 0.031,
+        0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.031, 0.061, 0.031, 0.031, 0.031, 0.0, 0.0, 0.031, 0.0, 0.0, 0.031,
+        0.0, 0.0, 0.0, 0.031, 0.0, 0.0, 0.0, 0.031, 0.0, 0.0, 0.0, 0.061, 0.031, 0.0, 0.0, 0.0, 0.031, 0.061, 0.031, 0.0,
+        0.031, 0.031, 0.061, 0.092, -0.009]]
         """
         sequence_list = get_data(data)
 
+        # Set and check physicochemical properties.
         if self.k == 2:
-            if added == 'all':
+            if all_property is True:
                 phyche_list = DIPYHYCHE_LIST
             else:
                 for e in phyche_list:
@@ -58,7 +74,7 @@ class PseKNC():
                         sys.stderr.write(error_info)
                         sys.exit(0)
         elif self.k == 3:
-            if added == 'all':
+            if all_property is True:
                 phyche_list = TRIPHYCHE_LIST
             else:
                 for e in phyche_list:
@@ -70,24 +86,43 @@ class PseKNC():
             sys.stderr.write("The value of k must be 2 or 3.")
             sys.exit(0)
 
+        # Set type and make vector.
         if vec_type == 1:
             from dnavec.pseknc.psekncutil import make_type1_vector
             vector = make_type1_vector(sequence_list, self.k, self.lamada, self.w, self.alphabet, phyche_list)
         elif vec_type == 2:
             from dnavec.pseknc.psekncutil import make_type2_vector
             vector = make_type2_vector(sequence_list, self.k, self.lamada, self.w, self.alphabet, phyche_list)
+        else:
+            error_info = 'Sorry, the type can only be 1 or 2, but your type is ' + str(vec_type) + '.'
+            sys.stderr.write(error_info)
+            sys.exit(0)
 
         return vector
 
 
 if __name__ == '__main__':
-    k = 3
-    lamada = 1
-    w = 0.05
-    vec_type = 1
-    pseKNC = PseKNC(k, lamada, w)
-    phyche_list = ['Bendability (DNAse)']
-    vector = pseKNC.make_vector(open('hs.txt'), vec_type, phyche_list)
+    pseKNC = PseKNC(k=2, lamada=1, w=0.05)
+    vector = pseKNC.make_vector(data=['GACTGAACTGCACTTTGGTTTCATATTATTTGCTC'], phyche_list=['Twist', 'Tilt'])
+    for e in vector:
+        print e
+        print len(e)
+    print len(vector)
+
+    vector = pseKNC.make_vector(data=['GACTGAACTGCACTTTGGTTTCATATTATTTGCTC'], vec_type=2, phyche_list=[], all_property=True)
+    for e in vector:
+        print e
+        print len(e)
+    print len(vector)
+
+    pseKNC = PseKNC(k=3, lamada=1, w=0.05)
+    vector = pseKNC.make_vector(data=['GACTGAACTGCACTTTGGTTTCATATTATTTGCTC'], phyche_list=['Dnase I'])
+    for e in vector:
+        print e
+        print len(e)
+    print len(vector)
+
+    vector = pseKNC.make_vector(data=['GACTGAACTGCACTTTGGTTTCATATTATTTGCTC'], vec_type=2, phyche_list=['Dnase I'])
     for e in vector:
         print e
         print len(e)
