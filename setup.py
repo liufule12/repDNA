3a80dfe1e52ba187c6d2437545ab6eac3403ab7d
liufule12 <liufule12@gmail.com>
2014-07-29 15:08:36 +0800

    add the ac package

--- setup.py
+++ setup.py
@@ -3,12 +3,10 @@ __author__ = 'aleeee'
 import os
 from distutils.core import setup
 
-
 setup(name='dnavec',
       version='0.1',
       description='DNA vectoring in Python.',
       long_description=open('README.md').read(),
       author='Fule Liu',
       author_email='liufule12@gmail.com',
-      url='https://github.com/liufule12/bio_package',
-      packages=['dnavec', 'dnavec.kmer', 'dnavec.psednc', 'dnavec.pseknc', 'dnavec.inucpseknc'],)
+      packages=['dnavec', 'dnavec.nac', 'dnavec.psenac', 'dnavec.ac'],)
