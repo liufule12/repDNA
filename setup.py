a01ff086856f4d9b205c37519e1b2b5f97f6cd18
liufule12 <liufule12@gmail.com>
2014-09-17 14:32:14 +0800

    change the package information

--- setup.py
+++ setup.py
@@ -3,11 +3,16 @@ __author__ = 'Fule Liu'
 from distutils.core import setup
 
 setup(name='repDNA',
-      version='0.1',
-      description='A Python package to generate various representations for DNA sequences',
-      long_description=open('README.md').read(),
-      author='Fule Liu',
-      author_email='liufule12@gmail.com',
+      version='1.0',
+      author='Bin Liu and Fule Liu',
+      author_email='bliu@insun.hit.edu.cn and liufule12@gmail.com',
+      maintainer='Fule Liu',
+      maintainer_email='liufule12@gmail.com',
+      url='bioinformatics.hitsz.edu.cn/repDNA/',
+      description='a Python package to generate various feature vectors of DNA sequences incorporating physicochemical properties and sequence-order effects',
+      download_url='bioinformatics.hitsz.edu.cn/repDNA/download',
+      platforms=['MS Windows', 'Mac OS', 'Unix/Linux'],
+      license='GPL',
       packages=['repDNA'],
       package_data={'repDNA': ['data/*.data', 'example/*.fasta', 'example/*.py']},
-      )
+)
